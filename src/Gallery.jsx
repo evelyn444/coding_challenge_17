7ac4908a761fea4448b65708d5f272afedece538
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-24 22:31:24 -0500

    Adding comments to gallery.jsx

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -4,31 +4,31 @@ function Gallery() {
     const [tours, setTours] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    const notInterested = (id)=> {
-        setTours((prevTours) => prevTours.filter((tour) => tour.id !== id))
+    const notInterested = (id)=> { // establishing not interested button/removing the tour
+        setTours((prevTours) => prevTours.filter((tour) => tour.id !== id)) 
     };
 
-    const toggleExpanded = (id)=> {
+    const toggleExpanded = (id)=> { // shows the expanded tour
         setTours((prevTours)=> prevTours.map((tour)=> tour.id === id ? {...tour, expanded: !tour.expanded}:tour ));
     };
 
 
-    useEffect(() => {
+    useEffect(() => { // use effect to show list of tours
         setLoading(true);
-        fetch('https://www.course-api.com/react-tours-project')
-        .then(response => response.json())
+        fetch('https://www.course-api.com/react-tours-project') // loads tours from api
+        .then(response => response.json()) 
         .then(data => {
-            setTours(data.map((tour) => ({...tour, expanded: false})));
+            setTours(data.map((tour) => ({...tour, expanded: false})));// adding the tours in with map
             setLoading(false);
         })
         .catch(error => {
-            console.error('There was an Error Retreiving the Tours', error);
+            console.error('There was an Error Retreiving the Tours', error);// error message if the fetch fails
             setLoading(false);
         });
     }, []);
 
-    if (loading){
-        return <div>Loading Available Tours</div>
+    if (loading){ 
+        return <div>Loading Available Tours</div> // loading message while fetching data
     }
     return (
         <div>
@@ -37,16 +37,16 @@ function Gallery() {
                 {tours.map(tour=>(
                     <li key={tour.id}>
                         {tour.name}- ${tour.price}
-                        <br></br>
+                        <br></br> 
                         <button onClick ={()=> toggleExpanded(tour.id)}>{tour.expanded ?'Show Less':'Read More'} </button>
-                        {tour.expanded && (
+                        {tour.expanded && ( // show more and show less buttons, and not interested button
                             <>
                             <p>{tour.info}</p>
-                            <img src={tour.image}></img>
+                            <img src={tour.image}></img> 
                             </>
                         )}
                         <br></br>
-                        <button onClick={()=> notInterested(tour.id)}>Not Interested</button>
+                        <button onClick={()=> notInterested(tour.id)}>Not Interested</button> 
                         <p></p>
                     </li>
 
